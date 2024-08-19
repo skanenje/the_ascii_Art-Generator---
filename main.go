688e095022506d0344502a0ed06efd56cdd06f56
skanenje <swapomuse@gmail.com>
2024-08-19 11:05:24 +0300

    Feat:
    add web app for the app

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"practice1/pkg"
@@ -10,8 +11,10 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	color, substring, argz, bannerFile, err := pkg.ParseFlag(args)
-	if err != nil || len(argz) < 1 || color == "" {
+
+	if checker(args){
+		color, substring, argz, bannerFile, err := pkg.ParseFlag(args)
+	if err != nil || len(argz) < 1 || color == "" || len(argz) == 3 {
 		fmt.Println("Usage: go run . [OPTION] [STRING]")
 		fmt.Println()
 		fmt.Println("EX: go run . --color=<color> <substring to be colored> \"something\"")
@@ -53,4 +56,20 @@ func main() {
 
 	// Use PrintAsciiArt instead of directly calling PrintArt
 	pkg.PrintAsciiArt(text, theMap, color, substring)
+	}else{
+		fmt.Println("Usage: go run . [OPTION] [STRING]")
+		fmt.Println()
+		fmt.Println("EX: go run . --color=<color> <substring to be colored> \"something\"")
+	}
+}
+
+func checker(sl []string) bool {
+	lastElement := sl[len(sl)-1]
+
+	switch lastElement {
+	case "shadow", "standard", "thinkertoy":
+		return true
+	default:
+		return !regexp.MustCompile(`^(sh.{4}|st.{6}|th.{9})$`).MatchString(lastElement)
+	}
 }
